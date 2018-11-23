ba858296daa0a999b5be6c4c1c46459500bc2955
Sathwik Tejaswi M <tejaswisathwik@gmail.com>
2018-11-22 18:52:35 -0600

    Update train.py

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import numpy as np
 import os
 import pickle
+from utils import *
 from data_loader import get_loader 
 from torchvision import transforms
 from build_vocab import Vocabulary
@@ -73,48 +74,14 @@ def main(arg):
 
 
 if __name__ == '__main__':
+    
     arg = {}
+    print('Use defualt args?(Y/N)')
+    ans = input()
     
-    print('model path :')
-    arg['model_path'] = input()
-    
-    print('crop size :')
-    arg['crop_size'] = int(input())
-    
-    print('vocab path :')
-    arg['vocab_path'] = input()
-    
-    print('image dir :')
-    arg['image_dir'] = input()
-    
-    print('caption path :')
-    arg['caption_path'] = input()
-    
-    print('log_step :')
-    arg['log_step'] = input()
-    
-    print('save_step :')
-    arg['save_step'] = int(input())
-    
-    print('embed_size :')
-    arg['embed_size'] = int(input())
-    
-    print('hidden_size :')
-    arg['hidden_size'] = int(input())
-    
-    print('num_layers :')
-    arg['num_layers'] = int(input())
-    
-    print('num_epochs :')
-    arg['num_epochs'] = int(input())
-    
-    print('batch_size :')
-    arg['batch_size'] = int(input())
-    
-    print('num_workers :')
-    arg['num_workers'] = int(input())
-    
-    print('learning_rate :')
-    arg['learning_rate'] = int(input())
+    if ans == 'Y':
+        arg = get_default()
+    else :
+        arg = get_args()
 
     main(arg)
