0f0e85884a24c0a961ca8deb822b05676dbc30bb
Sathwik Tejaswi M <tejaswisathwik@gmail.com>
2018-11-23 20:47:21 -0600

    Update resize.py

--- resize.py
+++ resize.py
@@ -23,20 +23,8 @@ def resize_images(image_dir, output_dir, size):
             print ("[{}/{}] Resized the images and saved into '{}'."
                    .format(i+1, num_images, output_dir))
 
-def main(args):
-    image_dir = args.image_dir
-    output_dir = args.output_dir
-    image_size = [args.image_size, args.image_size]
-    resize_images(image_dir, output_dir, image_size)
 
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--image_dir', type=str, default='./data/train2014/',
-                        help='directory for train images')
-    parser.add_argument('--output_dir', type=str, default='./data/resized2014/',
-                        help='directory for saving resized images')
-    parser.add_argument('--image_size', type=int, default=256,
-                        help='size for image after processing')
-    args = parser.parse_args()
-    main(args)
+image_dir = './data/train2014/'
+output_dir = './data/resized2014/'
+image_size = [256,256]
+resize_images(image_dir, output_dir, image_size)
